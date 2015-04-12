1813663beff824628de9bdb042e5db65169bbf81
Aaron Morris <aaorris@live.com>
2015-04-12 15:07:29 -0400

    mazegen in!

---
+++ vrvsworld/Assets/Tuna/Scripts/GeneratedRoomBlock.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+var walls:GameObject[];
+var activatedWalls:int[];
+
+//this runs in the editor, when things change
+function OnValidate() {
+	UpdateVisibility();
+}
+
+function UpdateVisibility() {
+	for(var i=0; i<walls.Length; i++) {
+		SetVisibility(walls[i],activatedWalls[i]);
+	}
+}
+
+//this will set the visibility of an object. (any object)
+function SetVisibility(target:GameObject, setting:int) {
+	//Let's actually delete them now that this is automatically generated.
+	if(setting==0) {
+		Destroy(target);
+	}
+	//target.GetComponent.<Renderer>().enabled=setting?true:false;
+	//target.GetComponent.<Collider>().enabled=setting?true:false;
+}
